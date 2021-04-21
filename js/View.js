ce9b9a4dee56c6d4acf0485c292487c6708be2d1
Emy Ifeora <emy@meetkumba.com>
2021-04-21 13:44:53 +0100

    Fixed Navigation Issues

--- js/View.js
+++ js/View.js
@@ -5,10 +5,10 @@ document.querySelector('#nav').innerHTML +=`<nav>
                 <i class="fas fa-times" onclick="hideMenu()"></i>
                 <ul>
                     <li><a href="index.html">Home</a></li>
-                    <li><a href="menu/about.html">About</a></li>
-                    <li><a href="menu/course.html">Course</a></li>
-                    <li><a href="menu/blog.html">Blog</a></li>
-                    <li><a href="menu/contact.html">Contact</a></li>
+                    <li><a href="/menu/about.html">About</a></li>
+                    <li><a href="/menu/course.html">Course</a></li>
+                    <li><a href="/menu/blog.html">Blog</a></li>
+                    <li><a href="/menu/contact.html">Contact</a></li>
                 </ul>
             </div>
             <i class="fas fa-bars" onclick="showMenu()"></i>
